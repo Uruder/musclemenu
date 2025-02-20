22320be80ca598f3f3bc613744e58527cbfde197
Uruder <Wtkband1@gmail.com>
2025-02-20 19:20:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 
