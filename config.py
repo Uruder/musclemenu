73aa953aaa9c43e793eecd19d1feb12f7cbd8a6b
Uruder <Wtkband1@gmail.com>
2025-02-28 10:19:40 +0200

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import os
-from dotenv import load_dotenv
-
-# Загружаем переменные окружения из .env
-load_dotenv()
-
-TOKEN = os.getenv("TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
